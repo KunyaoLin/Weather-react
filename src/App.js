415d7009b82c50f1776093058a4b048431d7f922
KunyaoLin <kunyaolin31@gmail.com>
2024-08-14 18:29:58 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import SearchBar from "./components/SearchBar";
 import CheckBox from "./components/CheckBox";
 import TempertureBtn from "./components/TempertureBtn";
 import WeatherCard from "./components/WeatherCard";
-
+// w-custom-max h-custom-max
 function App() {
   return (
-    <div className="flex justify-center items-center w-custom-max h-custom-max fixed">
-      <div className="flex flex-col space-y-6 h-custom-80 w-custom-80 border justify-center items-center border-gray-200 mt-2">
+    <div className=" bg-blue-200 flex justify-center items-center h-full w-full fixed">
+      <div className="flex flex-col rounded-xl space-y-6 h-custom-80 w-custom-80 border justify-center items-center bg-gray-300 border-gray-200 mt-2 shadow-md">
         <div className="font-semibold flex justify-center flex-col items-center mt-2">
           <p className="text-xl text-gray-500"> Check the Weather Forcast</p>
         </div>
